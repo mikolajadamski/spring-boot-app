0d3abe87081d4e81fa2bef58bd6f982d8935ef68
mikolajadamski <mikolaj.adamski47@gmail.com>
2020-12-11 17:33:20 +0100

    added java files

--- src/main/java/com/mikolajadamski/demo/student/StudentController.java
+++ src/main/java/com/mikolajadamski/demo/student/StudentController.java
@@ -1,31 +1,29 @@
 package com.mikolajadamski.demo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("students")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getAllStudents() {
-        return List.of(
-                new Student(
-                        UUID.randomUUID(),
-                        "James",
-                        "Bond",
-                        "jamesbond@gmail.com",
-                        Student.Gender.MALE),
-                new Student(
-                        UUID.randomUUID(),
-                        "Elisa",
-                        "Tamara",
-                        "elisatamara@hotmail.com",
-                        Student.Gender.FEMALE)
-        );
+        return studentService.getAllStudents();
     }
+
+    @PostMapping
+    public void addNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
 }
